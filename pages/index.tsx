f87a0312e042d502b4d7aed075c2a930ef277f7d
zarkuslol <lucasferlima8@gmail.com>
2022-08-07 21:17:15 -0300

    Commit inicial

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Counter } from "../components/Counter";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
+
+export default Home;
